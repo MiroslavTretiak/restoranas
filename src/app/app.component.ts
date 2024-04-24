e61b72f83dd1520b65fc463e50d1439eb8b5e45d
Miroslav <157689323+MiroslavTretiak@users.noreply.github.com>
2024-04-24 22:22:59 +0300

    Restoranas angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddProductComponent, ListProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
